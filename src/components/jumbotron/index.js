446a17a98d227d74838049b66f4df3ae45660d20
Sean Charoutanh <scharoutanh@gmail.com>
2021-05-25 17:45:48 -0600

    Add Jumbotron to a container, map data, styled component, update folder location

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Item, Inner } from './styles/jumbotron'
+import { Container, Item, Inner, Title, SubTitle, Image, Pane} from './styles/jumbotron'
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
@@ -11,4 +11,20 @@ export default function Jumbotron({ children, direction = 'row', ...restProps })
 
 Jumbotron.Container = function JumbotronContainer({ children, ...resProps }) {
     return <Container {...resProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...resProps }) {
+    return <Title {...resProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps} />
 }
