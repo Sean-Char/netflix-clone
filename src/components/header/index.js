f5628e22ee190d80d54d1ae5e018157b2d76824f
Sean Charoutanh <scharoutanh@gmail.com>
2021-06-05 22:24:45 -0600

    Added Feature and search box to header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Group, Background, Link, Text, Container, Logo, ButtonLink } from './styles/header'
+import { 
+    Group, 
+    Background, 
+    Link, 
+    Text, 
+    Container, 
+    Logo, 
+    ButtonLink,
+    Feature,
+    FeatureCallOut,
+    PlayButton,
+    Search,
+    SearchIcon,
+    SearchInput
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -12,6 +26,24 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
     return <Group {...restProps}>{children}</Group>;
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+    const [searchActive, setSearchActive] = useState(false);
+    
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(!searchActive)}>
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={({ target }) => setSearchTerm(target.value)}
+                placeholder="Search files and series"
+                active={searchActive}
+            />
+        </Search>
+    )
 }
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -32,4 +64,16 @@ Header.Link = function HeaderLink({ children, ...restProps }) {
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>;
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
 }
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -8,7 +8,9 @@ import { FooterContainer } from './footer';
 export function BrowseContainer() {
     const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
-
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    
     const { firebase } = useContext(FirebaseContext);
     
     const user = {
@@ -18,24 +20,35 @@ export function BrowseContainer() {
     
     return profile.displayName ? (
         <>
-            <Header>
+            <Header src="joker1" dontShowOnSmallViewPort>
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
                         <Header.Link 
-                            active={category === 'series'? 'true': 'false'}
-                            onClick={() => setCategory('series')}
-                        >
+                            active={category === 'series' ? 'true' : 'false'}
+                            onClick={() => setCategory('series')}>
                             Series
                         </Header.Link>
-                        <Header.Link
+                        <Header.Link 
                             active={category === 'films' ? 'true' : 'false'}
-                            onClick={() => setCategory('films')}
-                        >
+                            onClick={() => setCategory('films')}>
                             Films
                         </Header.Link>
                     </Header.Group>
+                    <Header.Group>
+                        <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    </Header.Group>
                 </Header.Frame>
+                
+                <Header.Feature>
+                    <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                    <Header.Text>
+                    Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+                    City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+                    futile attempt to feel like he's part of the world around him.
+                    </Header.Text>
+                    <Header.PlayButton>Play</Header.PlayButton>
+                </Header.Feature>
             </Header>
             <FooterContainer />
         </>)
