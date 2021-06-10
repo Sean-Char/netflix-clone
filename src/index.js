de932936a666f6aff699d94e42752a1a5d7d6cde
Sean Charoutanh <scharoutanh@gmail.com>
2021-06-10 16:28:46 -0600

    created auth listener for local storage, created protect routes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import { FirebaseContext } from './context/firebase';
 
 
 const config = {
-    apiKey: 'AIzaSyDWNMYLM9tMc8RuUtbe6GgSCOZ9frF3xZs',
-    authDomain: 'netflix-wanabe.firebaseapp.com',
-    projectId: 'netflix-wanabe',
-    storageBucket: 'netflix-wanabe.appspot.com',
-    messagingSenderId: '163761610992',
-    appId: '1:163761610992:web:9d0752790609db35d8a235'
+    
 };
 
 const firebase = window.firebase.initializeApp(config);
