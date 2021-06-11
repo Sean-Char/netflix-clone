a88f4a5ba40852d0c462ed93cb1bb5d69310a194
Sean Charoutanh <scharoutanh@gmail.com>
2021-06-11 13:05:38 -0600

    update keys

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './App';
 import { FirebaseContext } from './context/firebase';
+import * as KEYS  from './config';
 
-
-const config = {
-    
-};
+export const config = {
+    apiKey: KEYS.MY_API_KEY,
+    authDomain: KEYS.MY_DOMAIN,
+    projectId: KEYS.MY_PROJECT,
+    storageBucket: KEYS.MY_BUCKET,
+    messagingSenderId: KEYS.MY_SENDER_ID,
+    appId: KEYS.MY_SENDER_ID
+}
 
 const firebase = window.firebase.initializeApp(config);
 
