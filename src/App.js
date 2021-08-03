b65f53f863c9c6d69c5384130c90875e218498fd
Sean Charoutanh <scharoutanh@gmail.com>
2021-08-02 22:44:41 -0600

    Revert "Merge pull request #26 from Sean-Char/work-branch"
    This reverts commit 99029da44fa9e87ec7f3d64131d491797f9a85de, reversing
    changes made to 69a18d0d8407a9b9242c758f5d0db0fa32fd9a2e.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-//import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { Home, Signin, Signup, Browse } from './pages';
-import { useAuthListener } from './hooks';
-import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export function App() {
-    const { user } = useAuthListener();
-
   return (
     <Router>
         <Switch>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+            <Route path={ROUTES.SIGN_IN}>
                 <Signin />
-            </IsUserRedirect>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+            </Route>
+            <Route path={ROUTES.SIGN_UP}>
                 <Signup />
-            </IsUserRedirect>
-            <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+            </Route>
+            <Route path={ROUTES.BROWSE}>
                 <Browse />
-            </ProtectedRoute>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+            </Route>
+            <Route path={ROUTES.HOME}>
                 <Home />
-            </IsUserRedirect>
+            </Route>
         </Switch>
     </Router>
   );
